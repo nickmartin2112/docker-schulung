a7e451ffbfca4af6e2ef8f71a0416f4a495fde56
Dominik Martin <64202545+nickmartin2112@users.noreply.github.com>
2023-12-05 11:17:22 +0100

    Add

--- dockerfile/basic-1/php-code/index.php
+++ dockerfile/basic-1/php-code/index.php
@@ -1,4 +1,6 @@
 <?php
+
 echo "Das ist ein " . getenv('IMPERIUM') . " <br>";
 echo "Hello, World from Docker! My Name is " . getenv('USER') . " <br>";
+echo "Der kurs ist " . getenv('KURS') . " <br>";
 ?>
